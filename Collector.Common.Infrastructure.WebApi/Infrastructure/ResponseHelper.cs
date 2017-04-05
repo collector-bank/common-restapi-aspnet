8e68ae25b601299fb6831077cce0cb9dabc0cbe2
martin.bergkvist <Martin.bergqvist@collectorbank.se>
2017-04-05 08:38:13 +0200

    Added Extension method for building responses.

---
+++ Collector.Common.Infrastructure.WebApi/Infrastructure/ResponseHelper.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ResponseHelper.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Collector.Common.Infrastructure.WebApi.Infrastructure
+{
+    using System.Net;
+    using System.Net.Http;
+
+    using Collector.Common.RestContracts;
+
+    public static class ResponseHelper
+    {
+        public static HttpResponseMessage BuildResponse<T>(this HttpRequestMessage request, T data, HttpStatusCode status = HttpStatusCode.OK, string apiVersion = null)
+        {
+            var response = new Response<T>
+            {
+                ApiVersion = apiVersion,
+                Data = data
+            };
+
+            return request.CreateResponse(status, response);
+        }
+    }
+}
